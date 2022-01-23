a64d8f5884fb336d0adbac4acdeee3f143e25b76
Eduardo <epalma.externo@coopeuch.cl>
2022-01-23 13:49:31 -0300

    add progress to nullish

--- nullish_operators_and_or.js
+++ nullish_operators_and_or.js
@@ -3,18 +3,71 @@
 // referencia https://www.youtube.com/watch?v=anoXxubzh6w
 
 // ||=
-// Si el valor es falsy (undefined, null, 0, '', NaN o false), devuelve un valor (es mas estricto).
+// Si el valor  es(undefined, null, 0, '', NaN o false), devuelve un valor (es mas estricto).
 
 // ??=
-// Si el valor es null o undefined, devuelve un valor.
+// Si el valor es : null o undefined, devuelve un valor.
 
-const handleUrl = (history = null, url = null) => {
-     const checkHistory = history || false;
-     const checkUrl = url || false
-     checkHistory && checkUrl ? { history, url } : false;
-};
+// const handleUrl = (history = null, url = null) => {
+//   const history = history || "DESCONOCIDO";
+//   const url = url || "DESCONOCIDO";
+//   //return checkHistory && checkUrl ? { history, url } : false;
+//   return { history, url };
+// };
 
-const history = ''
-const url= ''
+// const history = "";
+// const url = "1";
 // handleUrl(history, url)
-  const {history,url} = handleUrl(history, url)
+// console.log(handleUrl(history, url));
+
+// class Persona {
+//      constructor(datos) {
+//        this.nombre = datos.nombre;
+//        this.segundoNombre = datos.segundoNombre ?? 'DESCONOCIDO';
+//        this.apellido = datos.apellido;
+//        this.edad = datos.edad;
+//        this.hijos = datos.hijos ??'DESCONOCIDO';
+//        this.licencia = datos.identificaciones?.licencia ??'DESCONOCIDO';
+//      }
+
+//      print() {
+//        console.log(
+//          `Nombre: ${this.nombre}`,
+//          `Segundo Nombre: ${this.segundoNombre}`,
+//          `Apellido: ${this.apellido}`,
+//          `Edad: ${this.edad}`,
+//          `Numero de Hijos: ${this.hijos}`,
+//          `Numero de Licencia: ${this.licencia}`
+//        );
+//      }
+//    }
+
+//    var persona = new Persona({
+//      nombre: 'Jose',
+//      segundoNombre: null,
+//      apellido: 'Gomez',
+//      edad: 18,
+//      hijos: null,
+//    //   identificaciones: {
+//    //     //licencia: 1324143,
+//    //     id: 12131231
+//    //   }
+//    });
+
+//    persona.print();
+
+// const cash = ''
+
+// const getCash = cash || 0
+
+// console.log(getCash);
+
+const getUserdefinedSettings = () => {
+     return {
+          difficylty: 'easy',
+          startingCash: 0
+     }
+}
+
+const cash = getUserdefinedSettings().startingCash ?? 500
+console.log(cash);
